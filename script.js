9af1a6709fdd66d672c6b57ebfdf6248ba6deec6
CarlosVieira1994 <cfiv1994@hotmail.com>
2023-05-10 13:40:48 -0300

    Atualização da sessão início

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ const logomarca = document.getElementById('logomarca');
 darkModeToggle.addEventListener('change', function() {
   if (darkModeToggle.checked) {
     stylesheet.href = 'darkmode.css';
-    logomarca.src = 'imagens/logo.png'
+    logomarca.src = 'imagens/logo.png';
+    imgbt.src = 'imagens/lua.png';
+    avatar.src = 'imagens/dark-avatar.png';
 
   } else {
     stylesheet.href = 'lightmode.css';
-    logomarca.src = 'imagens/logo-preta.png'
+    logomarca.src = 'imagens/logo-preta.png';
+    imgbt.src = 'imagens/sol.png';
+    avatar.src = 'imagens/light-avatar.png';
   }
 });
 // Alterna o menu entre aparente ou não aparente.
@@ -25,12 +29,12 @@ function clickMenu() {
 // Adiciona eventos de clique aos links do menu
 document.querySelectorAll('.menu-button a').forEach(function(link) {
   link.addEventListener('click', function(e) {
-    e.preventDefault(); // Previne o comportamento padrão do link
+    e.preventDefault();
     var target = this.getAttribute('href'); // Obtém o destino do link
     var targetElement = document.querySelector(target); // Obtém o elemento do destino
     var offset = targetElement.offsetTop; // Obtém a distância do elemento ao topo da página
     window.scrollTo({
-      top: offset,
+      top: offset -70,
       behavior: 'smooth' // Faz a rolagem suave
     });
     document.querySelector('.menu-button').style.display = 'none'; // Fecha o menu após clicar em um link
